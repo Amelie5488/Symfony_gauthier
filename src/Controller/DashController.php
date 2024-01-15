c6a394991d6e003344b6bddeb0da05ade0896256
Amelie5488 <leroyamelie@live.fr>
2024-01-15 16:50:45 +0100

    upload

--- src/Controller/DashController.php
+++ src/Controller/DashController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\PhotoType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashController extends AbstractController
 {
     #[Route('/dash', name: 'app_dash')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entity): Response
     {
+        $photo= new Photo();
+      
+        $form=$this->createForm(PhotoType::class, $photo);
+
+        $form -> handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $photo = $form->getData();
+      
+            
+
+            $directory = "../img";
+            $file = $form['lien']->getData();
+            $file->move($directory, $file->getClientOriginalName());
+            $photo->setLien($directory.'/'.$file->getClientOriginalName());
+
+            $entity->persist($photo);
+            $entity->flush();  
+
+            }    
         return $this->render('dash/index.html.twig', [
-            'controller_name' => 'DashController',
+            'form' => $form,
         ]);
-    }
-}
+    }}
+
---
+++ src/Form/PhotoType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Photo;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PhotoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                "attr" => ["placeholder" => "Nom de la photo", "class" => "w-[70%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]
+            ])
+            ->add('description', TextareaType::class, [
+                "attr" => ["placeholder" => "Description", "class" => "w-[70%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]
+            ])
+            ->add('lien', FileType::class, [
+                "attr" => ["placeholder" => "Ajouter une photo", "class" => "w-[70%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                "attr" => ["class"=>"w-[70%] h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"],
+                'choice_label' => 'nom', 
+                ])
+            
+                ->add('save', SubmitType::class, ['label' => 'Ajouter une photo', "attr"=>["class"=>" w-[70%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mb-5"]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Photo::class,
+        ]);
+    }
+}
