e1e170daef6e48f19816efa6b1963f4792bf858e
Amelie5488 <leroyamelie@live.fr>
2024-02-01 16:12:29 +0100

    update

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -23,7 +23,7 @@ class CommentaireType extends AbstractType
                 
 //'choice_label' => 'id',
            // ])
-            ->add('save', SubmitType::class, ['label' => 'Ajouter un commentaire', "attr"=>["class"=>" w-[60%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mb-5"]])
+            ->add('save', SubmitType::class, ['label' => 'Ajouter un commentaire', "attr"=>["class"=>" w-[100%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mb-5"]])
         ;
     }
 
